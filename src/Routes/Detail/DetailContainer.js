06a5d7df3f78c026bcff29bbd751b912f9b34957
Hannah0124 <hannahjoo24@gmail.com>
2019-11-22 14:47:22 -0800

    Detail Container (2)

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,30 +1,57 @@
 import React from 'react';
 import DetailPresenter from './DetailPresenter';
+import { moviesApi, tvApi } from 'api';
 
 export default class extends React.Component {
-	state = {
-		// I don't care if it is a movieDetail or showDetail, I'm going to get results
-		result: null,
-		error: null,
-		loading: true
-	};
+	constructor(props) {
+		super(props);
+		const { location: { pathname } } = props;
+		this.state = {
+			// I don't care if it is a movieDetail or showDetail, I'm going to get results
+			result: null,
+			error: null,
+			loading: true,
+			isMovie: pathname.includes('/movie/')
+		};
+	}
 
 	async componentDidMount() {
+		const { match: { params: { id } }, history: { push } } = this.props;
 
-		// Get "id" to get movies or shows information.
-		// Get "push" to use the method later.
-		const { match: { params: { id } }, history: {push} } = this.props;
+		const { isMovie } = this.state;
 
 		// parseInt Id to detect if it is number or NaN.
 		const parsedId = parseInt(id);
 
 		if (isNaN(parsedId)) {
-			return push("/");
+			return push('/');
+		}
+
+		let result = null;
+    
+		try {
+			if(isMovie) {
+				// If it is a movie, we are going to look for a movieDetail.
+				const request = await moviesApi.movieDetail(parsedId);
+				result = request.data;
+			} else {
+				const request = await tvApi.showDetail(parsedId);
+				result = request.data;
+			}
+
+			console.log(result);
+
+		} catch {
+			this.setState({error: "Can't find any results."})
+
+		} finally {
+			this.setState({loading: false, result})
 		}
 	}
 
 	render() {
 		const { result, error, loading } = this.state;
+		console.log(this.state);
 
 		return <DetailPresenter result={result} error={error} loading={loading} />;
 	}
