4d6ddcb5ff5e2e8a6e4ee29989f82474abe14740
Hannah0124 <hannahjoo24@gmail.com>
2019-11-22 11:58:45 -0800

    Detail Container (1)

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,7 +4,7 @@ import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
-import Detail from "Route/Detail";
+import Detail from "Routes/Detail";
 
 export default () => (
   <Router>
--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -2,16 +2,30 @@ import React from 'react';
 import DetailPresenter from './DetailPresenter';
 
 export default class extends React.Component {
-  state = {
-    // I don't care if it is a movieDetail or showDetail, I'm going to get results
-    result: null,
-    error: null,
-    loading: true
-  }
+	state = {
+		// I don't care if it is a movieDetail or showDetail, I'm going to get results
+		result: null,
+		error: null,
+		loading: true
+	};
 
-  render() {
-    const { result, error, loading} = this.state;
+	async componentDidMount() {
 
-    return <DetailPresenter result={result} error={error} loading={loading}/>;
-  }
+		// Get "id" to get movies or shows information.
+		// Get "push" to use the method later.
+		const { match: { params: { id } }, history: {push} } = this.props;
+
+		// parseInt Id to detect if it is number or NaN.
+		const parsedId = parseInt(id);
+
+		if (isNaN(parsedId)) {
+			return push("/");
+		}
+	}
+
+	render() {
+		const { result, error, loading } = this.state;
+
+		return <DetailPresenter result={result} error={error} loading={loading} />;
+	}
 }
